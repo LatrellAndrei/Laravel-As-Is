9fe3e4dc9b7f72283326e6c34f6161a713ba6c7f
unknown <CIT183E-28@M303.UC-BCF.EDU.PH>
2023-03-14 16:09:26 +0800

    new update

---
+++ resources/views/customer/index.blade.php
@@ -0,0 +1,40 @@
+
+@include('partials.header')
+<x-nav/>
+<table class="table table-hover">
+  <thead>
+    <tr>
+      <th scope="col">ID</th>
+      <th scope="col">Last Name</th>
+      <th scope="col">First Name</th>
+      <th scope="col">EMail</th>
+      <th scope="col">Address</th>
+      <th></th>
+      <th>
+
+      </th>
+    </tr>
+  </thead>
+  @if(Session::has('success'))
+    {{ Session::get('success') }}
+  @endif
+  @foreach($customers as $customer)
+     
+  <tbody>
+    <tr>
+      <th scope="row">{{$customer->id}}</th>
+      <td>{{$customer->lastName}}</td>
+      <td>{{$customer->firstName}}</td>
+      <td>{{$customer->email}}</td>
+      <td>{{$customer->address}}</td>
+      <td><a href="#">Edit</a></td>
+      <td>
+          <form action="delete/{{$customer->id}}">
+            <button type="submit" class="btn btn-danger">Delete</button>
+          </form>
+        </td>      
+    </tr>
+  </tbody>
+  @endforeach
+</table>
+@include('partials.footer')
